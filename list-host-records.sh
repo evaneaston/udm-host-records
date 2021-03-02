257fbe2c25d6d0ca1d8a9c7c783286a3ff0eef1c
Evan Easton <evan@eeaston.com>
2021-03-02 12:36:19 -0600

    * rename udm-add-host-record.sh to add-host-record.sh to match other scripts and README * breaking change: instead of 3rd arg being true false, use -f to set registernonqualified=false * add -v option to specify ipv4 vs ipv6 addresses.  Version 1.9 of the UDM firmware requirest this.  It defaults to 4.  There is no autodetection of the version type from the ip specified as an argument. * list host records at end of add-host-record.sh * include ipVersion in list-host-records.sh output

--- list-host-records.sh
+++ list-host-records.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 . $(dirname "$0")/common.sh
-$cmd GET -r /services | jq ".dnsForwarder.hostRecords[] | {address: .address.address, hostName, registerNonQualified}"
+$cmd GET -r /services | jq ".dnsForwarder.hostRecords[] | {address: .address.address, hostName, registerNonQualified, ipVersion: .address.ipVersion}"
